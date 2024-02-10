1bb42f0064ba7df88fa0b5a5f5faa04549f42f10
Outsider <87246847+outsider31000@users.noreply.github.com>
2024-02-10 09:17:33 +0000

    added versioning files

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -19,3 +19,8 @@ files {
 	'html/js/app.js',
 	'html/fonts/*ttf',
 }
+
+version '1.0'
+vorp_checker 'yes'
+vorp_name '^4Resource version Check^3'
+vorp_github 'https://github.com/VORPCORE/vorp_menu'
