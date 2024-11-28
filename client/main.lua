33481e2c07c659fee387ba1dd440c7d785da632c
Ssomemore <96509873+SsomeMore@users.noreply.github.com>
2024-11-29 02:38:24 +0700

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -3,39 +3,54 @@ MenuData.Opened = {}
 MenuData.RegisteredTypes = {}
 MenuData.LastSelectedIndex = {}
 
-MenuData.RegisteredTypes['default'] = {
-    open  = function(namespace, name, data)
+-- Tipe menu default
+MenuData.RegisteredTypes[Config.defaultMenuType] = {
+    open = function(namespace, name, data)
+        if not data then return end -- Cek nil
         SendNUIMessage({
             ak_menubase_action = 'openMenu',
             ak_menubase_namespace = namespace,
             ak_menubase_name = name,
-            ak_menubase_data = data
+            ak_menubase_data = data,
+            show = true
         })
+        -- Aktifkan kursor saat menu terbuka
+        SetNuiFocus(Config.enableCursor, Config.useMenuWithMouse)
+        SetNuiFocusKeepInput(Config.keepInputEnabled)
+        -- Trigger event saat menu terbuka
+        TriggerEvent('vorp_menu:opened', namespace, name, data)
     end,
     close = function(namespace, name)
         SendNUIMessage({
             ak_menubase_action = 'closeMenu',
             ak_menubase_namespace = namespace,
             ak_menubase_name = name,
-            -- ak_menubase_data = data
         })
+        -- Nonaktifkan kursor saat menu tertutup
+        SetNuiFocus(false, false)
+        SetNuiFocusKeepInput(false)
+        -- Trigger event saat menu tertutup
+        TriggerEvent('vorp_menu:closed', namespace, name)
     end
 }
 
 function MenuData.Open(type, namespace, name, data, submit, cancel, change, close)
-    local menu                            = {}
+    local menu = {}
+    menu.type = type
+    menu.namespace = namespace
+    menu.name = name
+    menu.data = data or {} -- Tambah tabel kosong default
+    menu.submit = submit
+    menu.cancel = cancel
+    menu.change = change
+    
+    -- Cek nil untuk menu.data
+    if menu.data then
+        menu.data.selected = MenuData.LastSelectedIndex[menu.type .. "_" .. menu.namespace .. "_" .. menu.name] or 0
+    end
 
-    menu.type                             = type
-    menu.namespace                        = namespace
-    menu.name                             = name
-    menu.data                             = data
-    menu.submit                           = submit
-    menu.cancel                           = cancel
-    menu.change                           = change
-    menu.data.selected                    = MenuData.LastSelectedIndex
-        [menu.type .. "_" .. menu.namespace .. "_" .. menu.name] or 0
-
-    menu.close                            = function()
+    menu.close = function()
+        if not MenuData.RegisteredTypes[type] then return end -- Cek nil
         MenuData.RegisteredTypes[type].close(namespace, name)
         for i = 1, #MenuData.Opened, 1 do
             if MenuData.Opened[i] then
@@ -44,40 +59,47 @@ function MenuData.Open(type, namespace, name, data, submit, cancel, change, clos
                 end
             end
         end
-
         if close then
             close()
         end
+        -- Trigger event saat menu ditutup via fungsi
+        TriggerEvent('vorp_menu:closed', namespace, name)
     end
 
-    menu.update                           = function(query, newData)
+    menu.update = function(query, newData)
+        if not menu.data or not menu.data.elements then return end -- Cek nil
         for i = 1, #menu.data.elements, 1 do
             local match = true
-
             for k, v in pairs(query) do
                 if menu.data.elements[i][k] ~= v then
                     match = false
                 end
             end
-
             if match then
                 for k, v in pairs(newData) do
                     menu.data.elements[i][k] = v
                 end
             end
         end
+        -- Trigger event saat menu diupdate
+        TriggerEvent('vorp_menu:updated', namespace, name, query, newData)
     end
 
-    menu.addNewElement                    = function(element)
+    menu.addNewElement = function(element)
+        if not menu.data.elements then menu.data.elements = {} end -- Inisialisasi jika nil
         menu.data.elements[#menu.data.elements + 1] = element
+        -- Trigger event saat elemen ditambahkan
+        TriggerEvent('vorp_menu:elementAdded', namespace, name, element)
     end
 
-    menu.removeElementByValue             = function(value, stop)
-        -- remove element from table
+    menu.removeElementByValue = function(value, stop)
+        if not menu.data.elements then return end -- Cek nil
         for i = 1, #menu.data.elements, 1 do
             if menu.data.elements[i] then
                 if menu.data.elements[i].value == value then
                     table.remove(menu.data.elements, i)
+                    -- Trigger event saat elemen dihapus
+                    TriggerEvent('vorp_menu:elementRemoved', namespace, name, value)
                     if stop then
                         break
                     end
@@ -86,12 +108,14 @@ function MenuData.Open(type, namespace, name, data, submit, cancel, change, clos
         end
     end
 
-    menu.removeElementByIndex             = function(index, stop)
-        -- remove element from table
+    menu.removeElementByIndex = function(index, stop)
+        if not menu.data.elements then return end -- Cek nil
         for i = 1, #menu.data.elements, 1 do
             if menu.data.elements[i] then
                 if i == index then
                     table.remove(menu.data.elements, i)
+                    -- Trigger event saat elemen dihapus
+                    TriggerEvent('vorp_menu:elementRemoved', namespace, name, index)
                     if stop then
                         break
                     end
@@ -100,28 +124,39 @@ function MenuData.Open(type, namespace, name, data, submit, cancel, change, clos
         end
     end
 
-    menu.refresh                          = function()
+    menu.refresh = function()
+        if not MenuData.RegisteredTypes[type] then return end -- Cek nil
         MenuData.RegisteredTypes[type].open(namespace, name, menu.data)
+        -- Trigger event saat menu direfresh
+        TriggerEvent('vorp_menu:refreshed', namespace, name)
     end
 
-    menu.setElement                       = function(i, key, val)
+    menu.setElement = function(i, key, val)
+        if not menu.data.elements then return end -- Cek nil
         menu.data.elements[i][key] = val
+        -- Trigger event saat elemen dimodifikasi
+        TriggerEvent('vorp_menu:elementModified', namespace, name, i, key, val)
     end
-    -- override all elements
-    menu.setElements                      = function(newElements)
+
+    menu.setElements = function(newElements)
         menu.data.elements = newElements
+        -- Trigger event saat elemen diset
+        TriggerEvent('vorp_menu:elementsSet', namespace, name, newElements)
     end
 
-    -- change the title of the current menu
-    menu.setTitle                         = function(val)
+    menu.setTitle = function(val)
         menu.data.title = val
+        -- Trigger event saat judul diubah
+        TriggerEvent('vorp_menu:titleChanged', namespace, name, val)
     end
 
-    menu.getElementByIndex                = function(index)
+    menu.getElementByIndex = function(index)
+        if not menu.data.elements then return nil end -- Cek nil
         return menu.data.elements[index]
     end
 
-    menu.getElementByValue                = function(value)
+    menu.getElementByValue = function(value)
+        if not menu.data.elements then return nil end -- Cek nil
         for i = 1, #menu.data.elements, 1 do
             if menu.data.elements[i].value == value then
                 return menu.data.elements[i]
@@ -129,12 +164,15 @@ function MenuData.Open(type, namespace, name, data, submit, cancel, change, clos
         end
     end
 
-    menu.removeElement                    = function(query)
+    menu.removeElement = function(query)
+        if not menu.data.elements then return end -- Cek nil
         for i = 1, #menu.data.elements, 1 do
             for k, v in pairs(query) do
                 if menu.data.elements[i] then
                     if menu.data.elements[i][k] == v then
                         menu.data.elements[i] = nil
+                        -- Trigger event saat elemen dihapus
+                        TriggerEvent('vorp_menu:elementRemoved', namespace, name, query)
                         break
                     end
                 end
@@ -143,8 +181,9 @@ function MenuData.Open(type, namespace, name, data, submit, cancel, change, clos
     end
 
     MenuData.Opened[#MenuData.Opened + 1] = menu
-    MenuData.RegisteredTypes[type].open(namespace, name, data)
-    PlaySoundFrontend("SELECT", "RDRO_Character_Creator_Sounds", true, 0)
+    if MenuData.RegisteredTypes[type] then -- Cek nil
+        MenuData.RegisteredTypes[type].open(namespace, name, data)
+    end
     return menu
 end
 
@@ -187,11 +226,20 @@ function MenuData.IsOpen(type, namespace, name)
 end
 
 function MenuData.ReOpen(oldMenu)
+    if not oldMenu then return end -- Cek nil
     MenuData.Open(oldMenu.type, oldMenu.namespace, oldMenu.name, oldMenu.data, oldMenu.submit, oldMenu.cancel,
         oldMenu.change, oldMenu.close)
 end
 
 local MenuType = 'default'
+
+-- Fungsi helper posisi mouse
+local function GetMousePosition()
+    local cursorX, cursorY = GetNuiCursorPosition()
+    local screenW, screenH = GetScreenResolution()
+    if not screenW or not screenH then return { x = 0, y = 0 } end -- Cek nil untuk resolusi
+    return { x = cursorX, y = cursorY }
+end
 
 RegisterNUICallback('menu_submit', function(data)
     PlaySoundFrontend("SELECT", "RDRO_Character_Creator_Sounds", true, 0)
@@ -200,7 +248,6 @@ RegisterNUICallback('menu_submit', function(data)
         menu.submit(data, menu)
     end
 end)
-
 
 RegisterNUICallback('playsound', function()
     PlaySoundFrontend("NAV_LEFT", "PAUSE_MENU_SOUNDSET", true, 0)
@@ -219,16 +266,32 @@ RegisterNUICallback('menu_change', function(data)
     if not menu then return end
     for i = 1, #data.elements, 1 do
         menu.setElement(i, 'value', data.elements[i].value)
-
-        if data.elements[i].selected then
-            menu.setElement(i, 'selected', true)
-        else
-            menu.setElement(i, 'selected', false)
-        end
+        menu.setElement(i, 'selected', data.elements[i].selected or false)
     end
-
     if menu.change ~= nil then
         menu.change(data, menu)
+        -- Trigger event saat menu berubah
+        TriggerEvent('vorp_menu:changed', data._namespace, data._name, data)
+    end
+end)
+
+RegisterNUICallback('mouse_click', function(data)
+    local menu = MenuData.GetOpened(MenuType, data._namespace, data._name)
+    if menu then
+        if Config.sounds.enable then
+            PlaySoundFrontend("SELECT", "RDRO_Character_Creator_Sounds", true, 0)
+        end
+        if data.selected ~= nil then
+            MenuData.LastSelectedIndex[menu.type .. "_" .. menu.namespace .. "_" .. menu.name] = data.selected
+            if menu.submit then
+                menu.submit(data, menu)
+                -- Trigger event saat item menu diklik
+                TriggerEvent('vorp_menu:clicked', data._namespace, data._name, data.selected)
+            end
+            if menu.change then
+                menu.change(data, menu)
+            end
+        end
     end
 end)
 
@@ -238,6 +301,8 @@ RegisterNUICallback('update_last_selected', function(data)
     local menuKey = menu.type .. "_" .. menu.namespace .. "_" .. menu.name
     if data.selected ~= nil then
         MenuData.LastSelectedIndex[menuKey] = data.selected
+        -- Trigger event saat last selected diupdate
+        TriggerEvent('vorp_menu:lastSelectedUpdated', data._namespace, data._name, data.selected)
     end
 end)
 
@@ -245,35 +310,79 @@ RegisterNUICallback('closeui', function(data)
     TriggerEvent("menuapi:closemenu")
 end)
 
-
 CreateThread(function()
     local PauseMenuState = false
-    local MenusToReOpen  = {}
+    local MenusToReOpen = {}
     while true do
         Wait(0)
         if #MenuData.Opened > 0 then
-            if (IsControlJustReleased(0, 0x43DBF61F) or IsDisabledControlJustReleased(0, 0x43DBF61F)) then
+
+           
+            -- Aktifkan kontrol mouse
+            EnableControlAction(0, 0xC7B5340A, true) -- Tombol kiri mouse
+            EnableControlAction(0, 0xF84FA74F, true) -- Kursor mouse
+            
+            -- Nonaktifkan kontrol kamera
+            DisableControlAction(0, 0x8FFC75D6, true) -- Nonaktifkan lihat kiri-kanan
+            DisableControlAction(0, 0xD2047988, true) -- Nonaktifkan lihat atas-bawah
+            DisableControlAction(0, 0x3D99EEC6, true) -- Nonaktifkan lihat mouse
+            DisableControlAction(0, 0xA987235F, true) -- Nonaktifkan lihat mouse
+            
+            -- Nonaktifkan OpenWheelMenu
+            DisableControlAction(0, 0xAC4BD4F1, true) -- Nonaktifkan OpenWheelMenu
+
+            -- Hanya nonaktifkan kontrol keyboard
+            DisableControlAction(0, Config.controls.select, true) -- Nonaktifkan Enter
+            DisableControlAction(0, Config.controls.back, true) -- Nonaktifkan Backspace
+            DisableControlAction(0, Config.controls.up, true) -- Nonaktifkan Panah Atas
+            DisableControlAction(0, Config.controls.down, true) -- Nonaktifkan Panah Bawah
+            DisableControlAction(0, Config.controls.left, true) -- Nonaktifkan Panah Kiri
+            DisableControlAction(0, Config.controls.right, true) -- Nonaktifkan Panah Kanan
+
+            
+            -- Kontrol keyboard
+            if IsDisabledControlJustPressed(0, Config.controls.select) then -- Enter
                 SendNUIMessage({ ak_menubase_action = 'controlPressed', ak_menubase_control = 'ENTER' })
             end
 
-            if (IsControlJustReleased(0, 0x308588E6) or IsDisabledControlJustReleased(0, 0x308588E6)) then
+            if IsDisabledControlJustPressed(0, Config.controls.back) then -- Backspace
                 SendNUIMessage({ ak_menubase_action = 'controlPressed', ak_menubase_control = 'BACKSPACE' })
             end
 
-            if (IsControlJustReleased(0, 0x911CB09E) or IsDisabledControlJustReleased(0, 0x911CB09E)) then
+            if IsDisabledControlJustPressed(0, Config.controls.up) then -- Panah Atas
                 SendNUIMessage({ ak_menubase_action = 'controlPressed', ak_menubase_control = 'TOP' })
             end
 
-            if (IsControlJustReleased(0, 0x4403F97F) or IsDisabledControlJustReleased(0, 0x4403F97F)) then
+            if IsDisabledControlJustPressed(0, Config.controls.down) then -- Panah Bawah
                 SendNUIMessage({ ak_menubase_action = 'controlPressed', ak_menubase_control = 'DOWN' })
             end
 
-            if (IsControlJustReleased(0, 0xAD7FCC5B) or IsDisabledControlJustReleased(0, 0xAD7FCC5B)) then
+            if IsDisabledControlJustPressed(0, Config.controls.left) then -- Panah Kiri
                 SendNUIMessage({ ak_menubase_action = 'controlPressed', ak_menubase_control = 'LEFT' })
             end
 
-            if (IsControlJustReleased(0, 0x65F9EC5B) or IsDisabledControlJustReleased(0, 0x65F9EC5B)) then
+            if IsDisabledControlJustPressed(0, Config.controls.right) then -- Panah Kanan
                 SendNUIMessage({ ak_menubase_action = 'controlPressed', ak_menubase_control = 'RIGHT' })
+            end
+
+            -- Lacak status hover
+            local mousePos = GetMousePosition()
+            local isHovered = false
+            SendNUIMessage({
+                ak_menubase_action = 'mouseMove',
+                mousePosition = mousePos,
+                trackHover = true
+            })
+
+            -- Penanganan klik mouse - hanya trigger jika hover
+            if IsControlJustPressed(0, 0xC7B5340A) then -- Tombol kiri mouse
+                if isHovered then
+                    SendNUIMessage({
+                        ak_menubase_action = 'controlPressed',
+                        ak_menubase_control = 'ENTER',
+                        mousePosition = mousePos
+                    })
+                end
             end
 
             if IsPauseMenuActive() then
@@ -288,12 +397,13 @@ CreateThread(function()
         else
             if PauseMenuState and not IsPauseMenuActive() then
                 PauseMenuState = false
-                Citizen.Wait(1000)
+                Wait(1000)
                 for k, v in pairs(MenusToReOpen) do
                     MenuData.ReOpen(v)
                 end
                 MenusToReOpen = {}
             end
+            Wait(500)
         end
     end
 end)
@@ -305,7 +415,6 @@ end)
 AddEventHandler("vorp_menu:getData", function(cb)
     return cb(MenuData)
 end)
-
 
 AddEventHandler('onClientResourceStart', function(resourceName)
     MenuData.LastSelectedIndex = {}
