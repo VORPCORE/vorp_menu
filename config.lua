ebed73579aae3a414ae8f166309025b73601e44e
Ssomemore <96509873+SsomeMore@users.noreply.github.com>
2024-11-29 02:37:02 +0700

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,18 @@
+Config = {}
+
+Config.debug = false                    -- Debug mode for development
+Config.defaultMenuType = 'default'      -- Default menu type to use
+Config.useMenuWithMouse = true         -- Enable/disable mouse control for menu
+Config.enableCursor = true             -- Enable cursor when menu is open
+Config.keepInputEnabled = true         -- Keep input enabled when menu is open
+
+
+Config.controls = {
+    up = 0x911CB09E,                   -- Up button (W)
+    down = 0x4403F97F,                 -- Down button (S) 
+    left = 0xAD7FCC5B,                 -- Left button (A)
+    right = 0x65F9EC5B,                -- Right button (D)
+    select = 0xC7B5340A,               -- Select button (Enter)
+    back = 0x156F7119                  -- Back button (Backspace)
+}
+
